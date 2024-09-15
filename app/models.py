3b15c65d2dfa91c2046a347aec8b926a78df660c
Bunyodbek <bunyodbekpirnazarov425@gmail.com>
2024-09-15 23:59:52 +0500

    news-master

--- app/models.py
+++ app/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
         verbose_name = "Kategoriya"
         verbose_name_plural = "Kategoriyalar"
 
+    def get_newses(self):
+        return News.objects.filter(category=self, is_active=True).order_by('-update')
+
 class News(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name="Yangilik nomi")
     slug = models.SlugField(max_length=255, verbose_name="Sluglar")
@@ -20,6 +23,13 @@ class News(models.Model):
     is_active = models.BooleanField(default=True, verbose_name="Saytga chiqarish")
     is_popular = models.BooleanField(default=True, verbose_name="Mashxur chiqarish")
     is_trending = models.BooleanField(default=True, verbose_name="Sara yangiliklar")
+    update = models.DateTimeField(auto_now_add=True, verbose_name="Yaratilgan vaqti")
+
+    def get_image(self):
+        if self.image:
+            return f'<img src="{self.image.url}" alt="">'
+        else:
+            return "https://answers-afd.microsoft.com/static/images/image-not-found.jpg"
 
     def __str__(self):
         return self.name
--- app/views.py
+++ app/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from .models import News, Category
 
 def home(request):
-    return render(request, 'index.html')
+
+    newses = News.objects.filter(is_active=True)
+    order_newses = newses.order_by('-update')
+    news_popular = newses.filter(is_popular=False).first()
+    news_top_story = newses.order_by("-created").first()
+    trending_news = newses.order_by("-created")[:8]
+    categories = Category.objects.all()
+    newses_all = order_newses[:8]
+
+    context = {
+        'newses_all': newses_all,
+        'news_popular': news_popular,
+        'news_top_story': news_top_story,
+        'trending_news': trending_news,
+        'categories': categories,
+    }
+
+    return render(request, 'index.html', context)
