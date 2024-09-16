8ae63f80258fb05a3a00fb3d20fcef6c5e4b175e
Bunyodbek <bunyodbekpirnazarov425@gmail.com>
2024-09-16 21:47:07 +0500

    news-master

--- app/views.py
+++ app/views.py
@@ -1,24 +1,6 @@
 from django.shortcuts import render
 from .models import News, Category
 
-# def home(request):
-#     newses = News.objects.all()
-#     order_newses = newses.order_by('-update')
-#     news_popular = newses.filter(is_active=True, is_popular=True).first()
-#     news_top_story = newses.order_by("-created").first()
-#     trending_news = newses.order_by("-created")[:8]
-#     categories = Category.objects.all()
-#     newses_all = order_newses[:6]
-#
-#     context = {
-#         'newses_all': newses_all,
-#         'news_popular': news_popular,
-#         'news_top_story': news_top_story,
-#         'trending_news': trending_news,
-#         'categories': categories,
-#     }
-#
-#     return render(request, 'index.html', context)
 
 def home(request):
     newses = News.objects.all()
